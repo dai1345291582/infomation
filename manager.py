e37e809d957731ab7a181e19c5af1211f75ab56f
dry <1345291582@qq.com>
2018-10-14 20:04:26 +0800

    生成数据库迁移文件

--- manager.py
+++ manager.py
@@ -1,6 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from info import db, create_app
+from info.models import *
 
 app = create_app('developement')
 manage = Manager(app)
