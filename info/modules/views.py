cde1263f523b7e9dc8cd202c54783d627e080478
dry <1345291582@qq.com>
2018-10-14 22:23:15 +0800

    图片验证码生成

--- info/modules/views.py
+++ info/modules/views.py
@@ -1,3 +1,4 @@
+from info.utils.captcha.captcha import captcha
 from . import index_blue
 from flask import render_template, current_app
 
@@ -10,3 +11,9 @@ def index():
 @index_blue.route('/favicon.ico')
 def web_logo():
     return current_app.send_static_file('news/favicon.ico')
+
+
+@index_blue.route('/image_code')
+def image_code():
+    name, text, image_code = captcha.generate_captcha()
+    return image_code
