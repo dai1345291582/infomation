cfc106b0496af7d8c4f11ff0d84b87caaaaa903b
dry <1345291582@qq.com>
2018-10-14 21:28:59 +0800

    首页页面渲染

--- info/modules/views.py
+++ info/modules/views.py
@@ -1,6 +1,12 @@
 from . import index_blue
+from flask import render_template, current_app
 
 
 @index_blue.route('/')
 def index():
-    return 'helloworld'
+    return render_template('index.html')
+
+
+@index_blue.route('/favicon.ico')
+def web_logo():
+    return current_app.send_static_file('news/favicon.ico')
